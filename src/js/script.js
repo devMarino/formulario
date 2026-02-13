28390517d5f02966e2183d0c82b59ba81c3a4a4c
devMarino <marcelomcb24@gmail.com>
2026-02-13 00:52:43 -0300

    add script.js = alert & password validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
-// em construção
+// form recebe id formulario
+const form = document.querySelector('.formulario')
+// evento de submit(apos o click do botao acontecera um evento)
+form.addEventListener('submit', evento => {
+    // evento pra n recarregar pagina
+    evento.preventDefault();
+
+    const password = document.querySelector('#password')
+    const confirmPassword = document.querySelector('#confirmPassword')
+    // validando as senhas
+    if (password.value !== confirmPassword.value){
+        alert('Inválido, senha incorreta')
+        return
+    }
+    alert("Formulário enviado")
+})
+
